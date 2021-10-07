867b9fca540c7988e9b2768a4256018cd64b4063
akaz <82073147+akazdayo@users.noreply.github.com>
2021-10-07 18:31:33 +0900

    Add files via upload

---
+++ 10sec.js
@@ -0,0 +1,27 @@
+var startTime = null;
+var displayArea = document.getElementById('display-area')
+function start() {
+    startTime = Date.now();
+    document.body.onkeydown = stop;
+}
+function stop() {
+    var currentTime = Date.now();
+    var seconds = (currentTime - startTime) / 1000;
+    if (9.5 <= seconds && seconds <= 10.5) {
+        if (confirm(seconds + "秒でした。すごい")) {
+            location.reload()
+        }
+    }
+    else {
+        if (confirm(seconds + "秒でした。残念")) {
+            location.reload()
+        }
+    }
+}
+
+if (confirm('OKを押して10秒だと思ったら何かキーを押してください')) {
+    start();
+}
+else {
+    location.reload()
+}
